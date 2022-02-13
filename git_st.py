9d56ecafd1f41a6e8c5edc3a52bbbcf27311bc3c
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-02-13 09:35:17 -0700

    write

--- git_st.py
+++ git_st.py
@@ -20,6 +20,7 @@ def git_call(token):
     os.system('git config --global pull.rebase false')
 
     os.system(f'git clone https://napoles-uach:{token}@github.com/napoles-uach/simple.git')
+    st.write(f'git clone https://napoles-uach:{token}@github.com/napoles-uach/simple.git')
     os.system('git fetch')
     os.system('git merge FETCH_HEAD')
 
