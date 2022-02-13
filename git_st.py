dcc7ebed1cbf62fcd9313968601ec478034f167e
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-02-12 19:41:44 -0700

    commit 4

--- git_st.py
+++ git_st.py
@@ -4,4 +4,5 @@ import os
 os.system('git config --global user.name "napoles-uach"')
 os.system('git config --global user.email "jnapoles@uach.mx"')
 os.system('git config --global pull.rebase false')
-os.system('git clone https://napoles-uach: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@github.com/napoles-uach/simple.git folder')
+os.system('git clone https://napoles-uach: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@github.com/napoles-uach/simple.git')
+os.system('git fetch')
