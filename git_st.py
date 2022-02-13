a76e574c7444f292135b9726f0cbb84829a5dfc2
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-02-12 19:44:47 -0700

    commit 10

--- git_st.py
+++ git_st.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        #print('file changed')
+        #print(os.popen("ls -l").read())
+        os.popen("git add app.py")
+        os.popen("git commit -m 'Fixes a bug.'")
+        os.popen("git push")
+
 st.title('Hi :cat: :+1:')
 os.system('git config --global user.name "napoles-uach"')
 os.system('git config --global user.email "jnapoles@uach.mx"')
@@ -7,3 +19,4 @@ os.system('git config --global pull.rebase false')
 os.system('git clone https://napoles-uach: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@github.com/napoles-uach/simple.git')
 os.system('git fetch')
 os.system('git merge FETCH_HEAD')
+
